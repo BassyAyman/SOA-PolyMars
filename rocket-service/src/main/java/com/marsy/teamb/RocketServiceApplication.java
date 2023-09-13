c27d3092ec6b43f097fa75564c241dd1331c6b6e
TobiasBonifay <tobias.bonifay@etu.unice.fr>
2023-09-13 15:58:14 +0200

    hotfix

--- rocket-service/src/main/java/com/marsy/teamb/RocketServiceApplication.java
+++ rocket-service/src/main/java/com/marsy/teamb/RocketServiceApplication.java
@@ -8,12 +8,12 @@ import java.util.logging.Logger;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 @SpringBootApplication
-public class CommandServiceApplication {
+public class RocketServiceApplication {
 
-    private static final Logger LOGGER = Logger.getLogger(CommandServiceApplication.class.getSimpleName());
+    private static final Logger LOGGER = Logger.getLogger(RocketServiceApplication.class.getSimpleName());
 
     public static void main(String[] args) {
-        SpringApplication.run(CommandServiceApplication.class, args);
+        SpringApplication.run(RocketServiceApplication.class, args);
     }
 
 }
