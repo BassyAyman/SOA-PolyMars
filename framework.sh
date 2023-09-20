52fc7e3c15d6cd1159019e076ab0364c3f21d89b
TobiasBonifay <tobias.bonifay@etu.unice.fr>
2023-09-20 16:53:47 +0200

    Test commit
    Signed-off-by: TobiasBonifay <tobias.bonifay@etu.unice.fr>

--- framework.sh
+++ framework.sh
@@ -6,7 +6,7 @@ function wait_on_health()  # $1 is URL of the Spring service with actuator on, $
 {
    until [ $(curl --silent "$1"/actuator/health | grep UP -c ) == 1 ]
    do
-      echo "Service $2 is not connected yet $1"
+      echo "Service $2 is not connected yet at $1"
       sleep 3
    done
    echo "Service $2 is up and running at $1"
