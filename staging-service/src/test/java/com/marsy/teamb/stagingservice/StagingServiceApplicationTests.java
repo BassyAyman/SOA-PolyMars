6b783b925c9834d620163a300401a1ed3eda425e
TobiasBonifay <tobias.bonifay@etu.unice.fr>
2023-09-20 16:40:36 +0200

    Staging-service added

---
+++ staging-service/src/test/java/com/marsy/teamb/stagingservice/StagingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marsy.teamb.stagingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StagingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
