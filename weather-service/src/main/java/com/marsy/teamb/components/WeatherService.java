cf16ea7ecff01a169d2b9c468a3a03e303d554e8
TobiasBonifay <tobias.bonifay@etu.unice.fr>
2023-09-20 11:06:24 +0200

    fakeweather :)

--- weather-service/src/main/java/com/marsy/teamb/components/WeatherService.java
+++ weather-service/src/main/java/com/marsy/teamb/components/WeatherService.java
@@ -3,6 +3,7 @@ package com.marsy.teamb.components;
 import com.marsy.teamb.interfaces.WeatherStatus;
 import org.springframework.stereotype.Controller;
 
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,10 +11,32 @@ import java.util.logging.Logger;
 public class WeatherService implements WeatherStatus {
 
     private static final Logger LOGGER = Logger.getLogger(WeatherService.class.getSimpleName());
+    private static final String[] WEATHER_CONDITIONS = {"Sunny", "Rainy", "Cloudy", "Windy", "Snowy"};
+
+    private static String currentWeather() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            LOGGER.log(Level.SEVERE, "Interrupted while simulating weather fetch delay", e);
+        }
+
+        // Simulate fetching weather data
+        Random rand = new Random();
+        int index = rand.nextInt(WEATHER_CONDITIONS.length);
+
+        String weather = WEATHER_CONDITIONS[index];
+        LOGGER.log(Level.INFO, "Current weather: " + weather);
+        return weather;
+    }
 
     @Override
     public String getWeather() {
-        LOGGER.log(Level.INFO, "Weather is good");
-        return "OK";
+        LOGGER.log(Level.INFO, "Fetching the weather...");
+
+        String weather = currentWeather();
+        if (weather.equals("Sunny"))
+            return "OK";
+
+        return "KO";
     }
 }
