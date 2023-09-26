bce39c0625b26914c8070e8caf5cb832ae549e59
TobiasBonifay <tobias.bonifay@etu.unice.fr>
2023-09-26 17:49:25 +0200

    should be ok
    Signed-off-by: TobiasBonifay <tobias.bonifay@etu.unice.fr>

--- staging-service/src/main/java/com/marsy/teamb/stagingservice/controllers/StagingController.java
+++ staging-service/src/main/java/com/marsy/teamb/stagingservice/controllers/StagingController.java
@@ -1,10 +1,12 @@
 package com.marsy.teamb.stagingservice.controllers;
 
 import com.marsy.teamb.stagingservice.components.dto.FuelDataDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,17 +18,23 @@ public class StagingController {
 
     private static final Logger LOGGER = Logger.getLogger(StagingController.class.getSimpleName());
 
+    // rest template
+    @Autowired
+    private static final RestTemplate restTemplate = new RestTemplate();
+
     @PostMapping(path = "fuelState", consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<String> shouldWeStage(@RequestBody FuelDataDTO fuelDto) {
         LOGGER.log(Level.INFO, "");
         ResponseEntity.ok("Not staging");
-        if (fuelDto.getFuelVolume() > 0.0) {
+        if (fuelDto.getFuelVolume() <= 8.1) {
             LOGGER.log(Level.INFO, "No fuel. You trash");
+            restTemplate.postForEntity("http://rocket-service:8080/staging", fuelDto, FuelDataDTO.class);
+            return ResponseEntity.ok("Staging");
         } else {
             LOGGER.log(Level.INFO, "Fuel..");
         }
-        // TODO
-        return ResponseEntity.ok("Staging");
+        restTemplate.postForEntity("http://rocket-service:8080/staging", fuelDto, FuelDataDTO.class);
+        return ResponseEntity.ok("not staged");
     }
 
 }
