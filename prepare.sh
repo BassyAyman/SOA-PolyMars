0cb696560a545d7131ba61c74598c4ca138f5a74
TobiasBonifay <tobias.bonifay@etu.unice.fr>
2023-09-27 14:40:55 +0200

    debug logs
    Signed-off-by: TobiasBonifay <tobias.bonifay@etu.unice.fr>

--- prepare.sh
+++ prepare.sh
@@ -28,7 +28,9 @@ if ! command -v docker &> /dev/null; then
     sudo systemctl enable --now docker
     sudo systemctl start docker
     sudo usermod -aG docker "$USER"
+    echo "gpasswd -a $USER docker"
     sudo gpasswd -a "$USER" docker
+    echo "Docker installed."
 else
     echo "Docker is already installed."
 fi
@@ -49,14 +51,14 @@ if ! command -v tmux > /dev/null; then
 fi
 
 
-function compile_dir()   # $1 is the dir to get it
-{
+function compile_dir() {
   echo "Preparing $1..."
     cd "$1"
     mvn clean install
     cd ..
 }
 
+echo "Compiling services..."
 compile_dir "weather-service"
 compile_dir "rocket-service"
 compile_dir "command-service"
@@ -66,10 +68,11 @@ compile_dir "telemetry-service"
 compile_dir "staging-service"
 compile_dir "satellite-service"
 compile_dir "booster-service"
+echo "Services compiled."
 
 echo "Starting Docker containers..."
 docker-compose up --build -d
-
+echo "Docker containers started."
 
 
 function wait_on_health()  # $1 is URL of the Spring service with actuator on, $2 is the service name
@@ -82,6 +85,7 @@ function wait_on_health()  # $1 is URL of the Spring service with actuator on, $
    echo "Service $2 is up and running at $1"
 }
 
+echo "Waiting for services to start..."
 wait_on_health http://localhost:8081 weather-service
 wait_on_health http://localhost:8082 rocket-service
 wait_on_health http://localhost:8083 command-service
@@ -91,3 +95,4 @@ wait_on_health http://localhost:8086 telemetry-service
 wait_on_health http://localhost:8087 staging-service
 wait_on_health http://localhost:8088 satellite-service
 wait_on_health http://localhost:8089 booster-service
+echo "All services are up and running."
