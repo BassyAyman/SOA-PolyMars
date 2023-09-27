901036e940d2ab1b7f61db5b67a6adec89bc2688
IgorMelnykSI <ihor.melnyk@etu.unice.fr>
2023-09-27 14:14:09 +0200

    First implem of data collect from satellite #9

--- payload-service/src/main/java/com/masy/teamb/payloadservice/interfaces/IPayloadProxy.java
+++ payload-service/src/main/java/com/masy/teamb/payloadservice/interfaces/IPayloadProxy.java
@@ -2,8 +2,11 @@ package com.masy.teamb.payloadservice.interfaces;
 
 public interface IPayloadProxy {
 
-    final static String ROCKET_SERVICE = "http://rocket-service:8080";
+    String ROCKET_SERVICE = "http://rocket-service:8080";
+    String SATELLITE_SERVICE = "http://satellite-service:8080";
 
     public void sendDetachOrder();
+
+    public void getSatelliteMetrics();
 
 }
