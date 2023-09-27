bdf22169316f416fa92df8b09bef72348e4c5df8
Mathieu Schalkwijk <mathieu.schalkwijk@etu.unice.fr>
2023-09-27 14:21:07 +0200

    Add booster service

--- windows-prepare.sh
+++ windows-prepare.sh
@@ -2,48 +2,6 @@
 
 set -e
 
-# Directory
-if [ ! -d "app" ]; then
-  mkdir app
-else
-  echo "Directory app already exists."
-fi
-
-# Check if Maven is installed; install if not
-if ! command -v mvn &> /dev/null; then
-    echo "Maven not found. Installing Maven..."
-    sudo apt update
-    sudo apt install -y maven
-else
-    echo "Maven is already installed."
-fi
-
-# Check if Docker is installed; install if not
-if ! command -v docker &> /dev/null; then
-    echo "Docker not found. Installing Docker..."
-    sudo apt update
-    sudo apt install -y docker.io
-    sudo systemctl enable --now docker
-else
-    echo "Docker is already installed."
-fi
-
-if ! command -v tmux > /dev/null; then
-  if command -v apt-get > /dev/null; then
-    sudo apt-get install -y tmux
-  elif command -v yum > /dev/null; then
-    sudo yum install -y tmux
-  elif command -v pacman > /dev/null; then
-    sudo pacman -S -y tmux
-  elif command -v brew > /dev/null; then
-    brew install -y tmux
-  else
-    echo "No package manager found. Cannot install tmux."
-    exit 1
-  fi
-fi
-
-
 function compile_dir()   # $1 is the dir to get it
 {
   echo "Preparing $1..."
---
+++ windows-run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#if [ -e "follow-logs.sh" ]; then
+#  chmod +x follow-logs.sh
+#  if command -v gnome-terminal > /dev/null; then
+#    gnome-terminal -- bash -c "./follow-logs.sh; exec bash"
+#  elif command -v xterm > /dev/null; then
+#    xterm -e "./follow-logs.sh; bash"
+#  elif command -v konsole > /dev/null; then
+#    konsole -e "./follow-logs.sh; bash"
+#  elif command -v xfce4-terminal > /dev/null; then
+#    xfce4-terminal -e "./follow-logs.sh; bash"
+#  # Detect if running in WSL
+#  elif grep -q Microsoft /proc/version; then
+#    # In WSL, just run the script in the current terminal
+#    ./follow-logs.sh
+#  # Detect if running in Git Bash
+#  elif [ ! -z "$MSYSTEM" ]; then
+#    start bash -c "./follow-logs.sh; exec bash"
+#  else
+#    echo "No known terminal emulator found. Cannot open a new terminal."
+#  fi
+#else
+#  echo "follow-logs.sh not found."
+#fi
+
+################################
+
+echo "Test of rocket launch: call Command Service to launch rocket"
+curl http://localhost:8083/launch
