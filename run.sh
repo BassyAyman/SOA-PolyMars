569ba6df667269af9f329ab227f77b6e785765f0
TobiasBonifay <tobias.bonifay@etu.unice.fr>
2023-09-30 12:16:57 +0200

    it was try 1
    Signed-off-by: TobiasBonifay <tobias.bonifay@etu.unice.fr>

--- run.sh
+++ run.sh
@@ -1,49 +1,27 @@
 #!/bin/bash
 
-#if [ -e "follow-logs.sh" ]; then
-#  chmod +x follow-logs.sh
-#  if command -v gnome-terminal > /dev/null; then
-#    gnome-terminal -- bash -c "./follow-logs.sh; exec bash"
-#  elif command -v xterm > /dev/null; then
-#    xterm -e "./follow-logs.sh; bash"
-#  elif command -v konsole > /dev/null; then
-#    konsole -e "./follow-logs.sh; bash"
-#  elif command -v xfce4-terminal > /dev/null; then
-#    xfce4-terminal -e "./follow-logs.sh; bash"
-#  # Detect if running in WSL
-#  elif grep -q Microsoft /proc/version; then
-#    # In WSL, just run the script in the current terminal
-#    ./follow-logs.sh
-#  # Detect if running in Git Bash
-#  elif [ ! -z "$MSYSTEM" ]; then
-#    start bash -c "./follow-logs.sh; exec bash"
-#  else
-#    echo "No known terminal emulator found. Cannot open a new terminal."
-#  fi
-#else
-#  echo "follow-logs.sh not found."
-#fi
-
-################################
-
-echo "Test of rocket launch: call Command Service to launch rocket"
-curl http://localhost:8083/launch
-
-################################
-# kill old session if it exists
-tmux kill-session -t mysession
+set -f
+tmux kill-session -t mysession 2>/dev/null
 
 tmux new-session -d -s mysession
 
 tmux split-window -v
+tmux split-window -h
 tmux resize-pane -y 1
 tmux set -g mouse off
-tmux set -g mouse-select-pane off
-tmux set -g mouse-select-window off
 tmux set -g status off
 
-tmux send-keys -t mysession:0.0 './follow-logs.sh' Enter
+tmux send-keys -t mysession:0.0 './follow-logs.sh' C-m
 tmux send-keys -t mysession:0.1 C-l
-tmux send-keys -t mysession:0.1 './follow-metrics.sh' Enter
+tmux send-keys -t mysession:0.1 './follow-metrics.sh' C-m
+tmux send-keys -t mysession:0.2 C-l
+
+tmux send-keys -t mysession:0.2 'sleep 10' C-m
+tmux send-keys -t mysession:0.2 'echo "Test of rocket launch: call Command Service to launch rocket"' C-m
+tmux send-keys -t mysession:0.2 'curl http://localhost:8083/launch' C-m
+tmux send-keys -t mysession:0.2 'echo "Wait for 15 seconds to simulate a problem with the rocket..."' C-m
+tmux send-keys -t mysession:0.2 'sleep 15' C-m
+tmux send-keys -t mysession:0.2 'echo "Simulating a problem with the rocket..."' C-m
+tmux send-keys -t mysession:0.2 'curl -X PUT http://localhost:8082/mockProblem' C-m
 
 tmux attach -t mysession
