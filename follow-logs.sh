e5128a17755fd0b0e1584c8fb1eec8c0a7ce6066
TobiasBonifay <tobias.bonifay@etu.unice.fr>
2023-10-01 17:48:37 +0200

    what about sudo?
    Signed-off-by: TobiasBonifay <tobias.bonifay@etu.unice.fr>

--- follow-logs.sh
+++ follow-logs.sh
@@ -3,6 +3,6 @@
 clear
 echo "Following logs for all services..."
 # docker-compose logs --follow --since 0s
-if ! docker-compose up --build -d; then
+if ! docker-compose logs -f --tail 0; then
     sudo docker-compose logs -f --tail 0
 fi
