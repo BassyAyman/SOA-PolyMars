7ebdd1cd8b7d65f3f9fa0348d00d21d86135f6cf
TobiasBonifay <tobias.bonifay@etu.unice.fr>
2023-10-04 11:38:48 +0200

    NAN
    Signed-off-by: TobiasBonifay <tobias.bonifay@etu.unice.fr>

--- follow-metrics.sh
+++ follow-metrics.sh
@@ -2,30 +2,37 @@
 
 format_to_2f() {
     local input="$1"
-    # Check if the input is a valid float or integer
     if [[ $input =~ ^-?[0-9]+([.][0-9]+)?$ ]]; then
         printf "%.2f" "$input"
     else
-        # Return the original input or "N/A" or any default value
-        echo "$input"
+        echo "N/A"
     fi
 }
 
 send_get_request() {
-    response=$(curl -s http://localhost:8082/rocketMetrics)
+    response=$(curl -s --connect-timeout 5 http://localhost:8082/rocketMetrics)
 
-    # Extract the values
-    altitude=$(echo "$response" | sed -n 's/.*"altitude":\([^,]*\).*/\1/p')
-    velocity=$(echo "$response" | sed -n 's/.*"velocity":\([^,]*\).*/\1/p')
-    fuelVolume=$(echo "$response" | sed -n 's/.*"fuelVolume":\([^,]*\).*/\1/p')
-    elapsedTime=$(echo "$response" | sed -n 's/.*"elapsedTime":\([^,]*\).*/\1/p')
-    isFine=$(echo "$response" | sed -n 's/.*"isFine":\([^}]*\).*/\1/p')
+    # Check if the curl command was successful
+    if [ $? -ne 0 ]; then
+        altitude="Error"
+        velocity="Error"
+        fuelVolume="Error"
+        elapsedTime="Error"
+        isFine="Error"
+    else
+        # Extract the values
+        altitude=$(echo "$response" | sed -n 's/.*"altitude":\([0-9.-]*\).*/\1/p')
+        velocity=$(echo "$response" | sed -n 's/.*"velocity":\([0-9.-]*\).*/\1/p')
+        fuelVolume=$(echo "$response" | sed -n 's/.*"fuelVolume":\([0-9.-]*\).*/\1/p')
+        elapsedTime=$(echo "$response" | sed -n 's/.*"elapsedTime":\([0-9.-]*\).*/\1/p')
+        isFine=$(echo "$response" | sed -n 's/.*"isFine":\([^}]*\).*/\1/p')
 
-    # Format to two decimal places if possible
-    altitude=$(format_to_2f "$altitude")
-    velocity=$(format_to_2f "$velocity")
-    fuelVolume=$(format_to_2f "$fuelVolume")
-    elapsedTime=$(format_to_2f "$elapsedTime")
+        # Format to two decimal places if possible
+        altitude=$(format_to_2f "$altitude")
+        velocity=$(format_to_2f "$velocity")
+        fuelVolume=$(format_to_2f "$fuelVolume")
+        elapsedTime=$(format_to_2f "$elapsedTime")
+    fi
 
     # Display the result
     tput cr
