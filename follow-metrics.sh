d5be745d8ba38314e59766b7d2f02edfe81709f9
TobiasBonifay <tobias.bonifay@etu.unice.fr>
2023-10-04 11:22:40 +0200

    better display 0.2f
    Signed-off-by: TobiasBonifay <tobias.bonifay@etu.unice.fr>

--- follow-metrics.sh
+++ follow-metrics.sh
@@ -2,15 +2,24 @@
 
 send_get_request() {
     response=$(curl -s http://localhost:8082/rocketMetrics)
+
+    # Extract the values
     altitude=$(echo "$response" | sed -n 's/.*"altitude":\([^,]*\).*/\1/p')
     velocity=$(echo "$response" | sed -n 's/.*"velocity":\([^,]*\).*/\1/p')
     fuelVolume=$(echo "$response" | sed -n 's/.*"fuelVolume":\([^,]*\).*/\1/p')
     elapsedTime=$(echo "$response" | sed -n 's/.*"elapsedTime":\([^,]*\).*/\1/p')
     isFine=$(echo "$response" | sed -n 's/.*"isFine":\([^}]*\).*/\1/p')
+
+    # Format to two decimal places, capture the result, and keep as string
+    altitude=$(printf "%.2f" "$altitude")
+    velocity=$(printf "%.2f" "$velocity")
+    fuelVolume=$(printf "%.2f" "$fuelVolume")
+    elapsedTime=$(printf "%.2f" "$elapsedTime")
+
+    # Display the result
     tput cr
     printf "Altitude: %s m | Velocity: %s m/s | Fuel: %s m^3 | Elapsed Time: %s s | IsRocketFine: %s" "$altitude" "$velocity" "$fuelVolume" "$elapsedTime" "$isFine"
 }
-
 
 clear
 while true; do
