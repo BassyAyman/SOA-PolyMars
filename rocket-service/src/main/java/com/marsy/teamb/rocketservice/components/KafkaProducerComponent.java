37f16a170ad407895b341e03ff7bd277ca50f2f4
bassy <baberna2001@gmail.com>
2023-10-10 16:16:04 +0200

    kafka(webCasterLog): new config for new topic to rocket service #30

--- rocket-service/src/main/java/com/marsy/teamb/rocketservice/components/KafkaProducerComponent.java
+++ rocket-service/src/main/java/com/marsy/teamb/rocketservice/components/KafkaProducerComponent.java
@@ -11,7 +11,14 @@ public class KafkaProducerComponent {
     @Autowired
     private KafkaTemplate<String, MarsyLog> kafkaTemplate;
 
+    @Autowired
+    private KafkaTemplate<String, String> stringKafkaTemplate;
+
     public void sendToCommandLogs(String log){
         kafkaTemplate.send("commandLog", MarsyLog.builder().service("rocket").message(log).build());
+    }
+
+    public void sendMsgToWebCaster(String msg){
+        stringKafkaTemplate.send("messageToCaster", msg);
     }
 }
