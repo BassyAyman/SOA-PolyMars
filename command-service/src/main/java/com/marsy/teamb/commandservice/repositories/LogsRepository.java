f2371088323aa232f215c83e1de7d479e061d251
IgorMelnykSI <ihor.melnyk@etu.unice.fr>
2023-10-22 10:47:43 +0200

    Possibility to retry logs from Command Service DB using a new endpoint

--- command-service/src/main/java/com/marsy/teamb/commandservice/repositories/LogsRepository.java
+++ command-service/src/main/java/com/marsy/teamb/commandservice/repositories/LogsRepository.java
@@ -3,5 +3,10 @@ package com.marsy.teamb.commandservice.repositories;
 import com.marsy.teamb.commandservice.modele.MarsyLogForDB;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface LogsRepository extends MongoRepository<MarsyLogForDB, String> {
+
+    public List<MarsyLogForDB> getMarsyLogForDBByMissionID(String missionID);
+
 }
