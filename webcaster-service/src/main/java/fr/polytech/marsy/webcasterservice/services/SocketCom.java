3c814ef6f132708a4a7cb26cc2cf2850cd258e06
Mathieu Schalkwijk <mathieu.schalkwijk@etu.unice.fr>
2023-11-18 16:00:16 +0100

    Display interview on webcaster logs

--- webcaster-service/src/main/java/fr/polytech/marsy/webcasterservice/services/SocketCom.java
+++ webcaster-service/src/main/java/fr/polytech/marsy/webcasterservice/services/SocketCom.java
@@ -1,5 +1,6 @@
 package fr.polytech.marsy.webcasterservice.services;
 
+import fr.polytech.marsy.webcasterservice.logger.CustomLogger;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,6 +17,8 @@ import java.util.logging.Logger;
 @Service
 public class SocketCom {
     private static final Logger LOGGER = Logger.getLogger(SocketCom.class.getSimpleName());
+
+    private static final CustomLogger DISPLAY = new CustomLogger(WebCasterService.class);
 
     final static String ROCKET_SERVICE_URL = "http://rocket-service:8080";
 
@@ -34,8 +37,10 @@ public class SocketCom {
             ServerSocket serverSocket = new ServerSocket(3000);
             restTemplate.put(ROCKET_SERVICE_URL + "/startInterview", null);
             LOGGER.info("We are currently trying to establish a connection with Thomas Pasquier");
+            DISPLAY.log("We are currently trying to establish a connection with Thomas Pasquier");
             Socket clientSocket = serverSocket.accept();
             LOGGER.info("We finally established a connection with Thomas!");
+            DISPLAY.log("We finally established a connection with Thomas!");
 
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -44,13 +49,16 @@ public class SocketCom {
             for (String question : questions) {
                 Thread.sleep(2000);
                 LOGGER.info("Ms Caster says: " + question);
+                DISPLAY.log("Ms Caster says: " + question);
                 // send question in socket
                 out.println(question);
                 // display received answer
                 LOGGER.info("Thomas Pasquier says: " + in.readLine());
+                DISPLAY.log("Thomas Pasquier says: " + in.readLine());
             }
 
             LOGGER.info("We are done talking to Thomas, closing the connection");
+            DISPLAY.log("We are done talking to Thomas, closing the connection");
             out.println("close");
             out.close();
             in.close();
@@ -58,6 +66,7 @@ public class SocketCom {
 
         } catch (Exception e) {
             LOGGER.log(Level.SEVERE, "Error during socket communication:", e);
+            DISPLAY.log("We had a problem during the communication with Thomas Pasquier");
             e.printStackTrace();
         }
     }
