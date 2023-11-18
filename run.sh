70f94b7863451fa835d408f4e677272c4c052a24
TobiasBonifay <tobias.bonifay@etu.unice.fr>
2023-11-18 15:58:54 +0100

    TRY6
    Signed-off-by: TobiasBonifay <tobias.bonifay@etu.unice.fr>

--- run.sh
+++ run.sh
@@ -4,27 +4,36 @@ tmux kill-session -t mysession 2>/dev/null
 
 tmux new-session -d -s mysession
 
-# Split top pane horizontally to have log_displayIgor.sh on the left and log_webcaster.sh on the right
-tmux send-keys -t mysession:0.0 './scripts/log_displayIgor.sh' C-m
+# Top left pane for log_display_igor.sh
+tmux send-keys -t mysession './scripts/log_displayIgor.sh' C-m
+
+# Split top pane horizontally for log_webcaster.sh
 tmux split-window -h
-tmux send-keys -t mysession:0.1 './scripts/log_webcaster.sh' C-m
+tmux send-keys -t mysession './scripts/log_webcaster.sh' C-m
 
-# Create a new window pane at the bottom for follow-metrics.sh
-tmux select-pane -t mysession:0.0 # Select the first pane to make sure the split is full width
-tmux split-window -v
-tmux resize-pane -t mysession:0.2 -y 1 # Make the pane one line in height
-tmux send-keys -t mysession:0.2 './scripts/follow-metrics.sh' C-m
+# Navigate back to the first pane before splitting for bottom panes
+tmux select-pane -t mysession:0.0
 
-# Create a new window pane at the very bottom for log_astronaute.sh
-tmux select-pane -t mysession:0.2 # Select the pane above to ensure the split is full width
+# Create a new pane for follow-metrics.sh at the bottom
+tmux split-window -v -p 20
+tmux send-keys -t mysession './scripts/follow-metrics.sh' C-m
+
+# Resize bottom pane to a single line
+tmux resize-pane -t mysession:0.2 -y 1
+
+# Create another pane at the very bottom for log_astronaute.sh
+# Make sure to select the correct pane to split from, which should be follow-metrics.sh pane
+tmux select-pane -t mysession:0.2
 tmux split-window -v
-tmux resize-pane -t mysession:0.3 -y 1 # Make the pane one line in height
-tmux send-keys -t mysession:0.3 './scripts/log_astronaute.sh' C-m
+tmux send-keys -t mysession './scripts/log_astronaute.sh' C-m
+
+# Resize new bottom pane to a single line
+tmux resize-pane -t mysession:0.3 -y 1
 
 # Start the hidden session
 tmux new-session -d -s hidden_session
-tmux send-keys -t hidden_session:0.0 './scripts/scenario.sh' C-m
-tmux set -t hidden_session:0.0 pane-border-status off
+tmux send-keys -t hidden_session './scripts/scenario.sh' C-m
+tmux set -t hidden_session pane-border-status off
 
 tmux select-pane -t mysession:0.0 # Focus on Igor logs by default
 tmux attach -t mysession
